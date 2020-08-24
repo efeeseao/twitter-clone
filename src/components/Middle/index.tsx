d8b5ff9ea8c6921ced6024f259034cb349d4341b
Fernando dos Santos <nando4803@gmail.com>
2020-08-24 16:46:30 +0100

    This commit update styles

--- src/components/Middle/index.tsx
+++ src/components/Middle/index.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Header, BackIcon, ProfileInfo } from './styles';
 
 const Middle: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon />
+        </button>
+        <ProfileInfo>
+          <strong>Fernando</strong>
+          <span>1,333 Tweets</span>
+        </ProfileInfo>
+      </Header>
+      {/* <ProfilePage /> */}
+      {/* <ButtonMenu>
+        <HomeIcon />
+        <SearchIcon />
+        <BellIcon />
+        <EmailIcon />
+      </ButtonMenu> */}
+    </Container>
+  );
 };
 
 export default Middle;
