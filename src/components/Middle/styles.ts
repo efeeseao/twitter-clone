bdd4c49ae78b73256fa0c010c866c24ca675d2c1
Fernando dos Santos <nando4803@gmail.com>
2020-08-24 16:31:32 +0100

    This commit add middle styles

--- src/components/Middle/styles.ts
+++ src/components/Middle/styles.ts
@@ -1,3 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div``;
+
+export const Header = styled.header``;
+export const BackIcon = styled.div``;
+export const ProfileInfo = styled.div``;
